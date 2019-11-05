38d33d36c03dccc782e9aa4a132586b1a4a7aecd
thangtt <thangtt@rikkeisoft.com>
2019-11-05 11:12:45 +0700

    fix di

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,11 @@ exports.requires = [
 	'middlewares.auth'
 ];
 
-exports.factory = function (express, mongoose, auth) {
+exports.factory = function (express, auth) {
 	let router = express.Router();
 
-	router.get('/', auth.checkSignIn, async function (req, res, next) {
-		await res.render('index', {
+	router.get('/', auth.checkSignIn, function (req, res, next) {
+		res.render('index', {
 			title: 'Phonebook',
 			page: 'HOME',
 			signed: req.session.user ? true : false
